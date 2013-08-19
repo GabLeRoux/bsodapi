a31273b90e8de9c61f466c7f86135164b4b2693e
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2013-08-18 21:56:27 -0400

    Facebook Open Graph shouldn't cry now... take 2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ exports.index = function(req, res){
 
 	// App full url (Thanks stackoverflow)
 	var port = req.app.settings.port || cfg.port;
-	var full_url = req.protocol + '://' + req.host  + ( port == 80 || port == 443 || req.host == "herokuapp.com" ? '' : ':' + port );
+	var full_url = req.protocol + '://' + req.host  + ( port == 80 || port == 443 || req.host.indexOf("herokuapp.com") != -1 ? '' : ':' + port );
 	var full_req = full_url + req.path;
 
 	// Response stuff
