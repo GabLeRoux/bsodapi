9dc17342a355f9e6558371385b3c0b6bc64ca047
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2013-08-18 21:54:50 -0400

    Facebook Open Graph shouldn't cry now... take 2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ exports.index = function(req, res){
 
 	// App full url (Thanks stackoverflow)
 	var port = req.app.settings.port || cfg.port;
-	var full_url = req.protocol + '://' + req.host  + ( port == 80 || port == 443 ? '' : ':' + port );
+	var full_url = req.protocol + '://' + req.host  + ( port == 80 || port == 443 || req.host == "herokuapp.com" ? '' : ':' + port );
 	var full_req = full_url + req.path;
 
 	// Response stuff
@@ -19,6 +19,12 @@ exports.index = function(req, res){
 		errorMessage: error[1]
 	}
 
-	res.render('index', { response: response, host: full_url, url:full_req, title: "Something went wrong! - BSOD API" });
+	res.render('index', {
+		response: response,
+		host: full_url,
+		url:full_req,
+		title: "Something went wrong! - BSOD API",
+		description: "BSOD API made with node.js! Are you ready to buuuuuug just like in good old time? :)"
+	});
 	console.log("Retuned: ", response);
 };
